dd3b7ad8beedbbefe3421e06d37b5232d8bb93d5
Gus Bikos <gusbikos@gmail.com>
2021-01-15 20:11:34 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -28,7 +28,7 @@ def update_status
   epic_tragedy
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
 end
-  
+
 
 
 def add_characters
